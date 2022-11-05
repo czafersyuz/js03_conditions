dc4171c8e75f0e1518c46737b57f2fe94970969f
czafersyuz <czafersyuz@gmail.com>
2022-11-05 16:47:42 +0300

    boolean

--- string.js
+++ string.js
@@ -4,3 +4,25 @@
 //     x = x * x;
 //     alert(x);
 //   }
+
+
+// Bir sayının sıfıra bölünmesi 
+var x = 10 / 0;   // Infinity
+var y = -10 / 0;  // -Infinity
+console.log(x);
+console.log(y);
+
+//JavaScript Infinity anahtar kelimesinin veri türü number’dır.
+
+console.log(typeof Infinity);
+
+
+//tırnak işaretleri
+let firstName = `cafer`;
+let lastName = "sıcakyüz";
+let myCity = 'ankara';
+console.log(`I'm ${firstName} ${lastName}. I live in ${myCity}`);
+console.log("I'm " + firstName + " " + lastName + ". I live in " + myCity);
+
+
+//boolean veri tipleri
